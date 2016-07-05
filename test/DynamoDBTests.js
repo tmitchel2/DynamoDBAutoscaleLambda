93c94a89adcad7135a17c566f9db4cdd66d44e5f
Ryan McInteer <Ryan.McInteer@xero.com>
2016-07-06 10:19:42 +1200

    Tidy up the tests

--- test/DynamoDBTests.js
+++ test/DynamoDBTests.js
@@ -14,22 +14,22 @@ let expect = chai.expect;
 console.log('runnng tests');
 let db;
 describe('dynamodb', () => {
-  beforeEach(() => {
+  before(() => {
     db = DynamoDB.create(Region);
   });
 
   const wrapInPromise = data => ({promise: async () => ({ data: data }) });
-  describe('#listTablesToScaleAsync', () => {
-    let tableName = 'table1';
+
+  describe('#listTablesAsync', () => {
+    let tableNames = { TableNames: [ 'table1', 'table2', 'table3' ] };
     beforeEach(() => {
-      db = DynamoDB.create(Region);
-      const stubbedTables = { Items: [ { TableName:{ S: tableName } } ] } ;
-      sinon.stub(db._db, "scan", () => wrapInPromise(stubbedTables));
+      const stubbedTables = { Items: tableNames };
+      sinon.stub(db._db, "listTables", () => wrapInPromise(stubbedTables));
     });
 
     it("should return a list of tables to scale.", async function(){
-      let result = await db.listTablesToScaleAsync();
-      result.TableNames[0].should.equal(tableName);
+      let result = await db.listTablesAsync();
+      result.Items.should.equal(tableNames);
     });
   });
 });
